beb7944e3ca57f4871526a72d5ed0f374ec1efb5
leandrolannister <leandrohendrix@gmail.com>
2021-01-07 08:26:14 -0300

    Estrutura de pastas e Classe Neg

--- client/js/index.js
+++ client/js/index.js
@@ -17,6 +17,7 @@ document.querySelector('.form').addEventListener('submit', (event) =>{
      td.textContent = campo.value;
      tr.appendChild(td);
    });
+   
    tdVolume.textContent = campos[1].value * campos[2].value;
    tr.appendChild(tdVolume);
 
